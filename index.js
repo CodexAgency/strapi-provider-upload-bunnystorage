263b8702101e6e2ab6c1cef16d09ce6f2dfcaa34
Carmine Milieni <carmine.milieni@gmail.com>
2021-09-01 12:41:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,44 +4,136 @@ const axios = require('axios');
 
 module.exports = {
   init(config) {
-    const storageEndpoint = `${config.region}/${config.storageZone}/`;
+
+
+    let storageEndpoint = `${config.storageHost}/${config.storageZone}/`;
+
+    if (config.storageFolder != null) {
+      storageEndpoint += config.storageFolder + '/';
+    }
+
+    let videoEndpoint = 'http://video.bunnycdn.com/library/' + config.videostreamLibraryId + '/videos';
+
+
+    function isVideo(file) {
+      const types = [".mpeg", ".mp4", ".m4v", ".wmv", ".avi", ".flv",];
+      let video = false;
+
+      Object.values(types).forEach((type) => {
+        if (file.ext === type) video = true;
+
+        return video;
+      });
+
+      return video;
+    }
 
     return {
       upload(file) {
-        return new Promise((resolve, reject) => {
-          axios.put(`${storageEndpoint}${file.hash}${file.ext}`,
-            file.buffer,
-            {headers: {'AccessKey': config.apiKey}})
-            .then(function (response) {
-              if (response.data.HttpCode === 201) {
-                file.url = `${config.pullZone}/${file.hash}${file.ext}`;
+        if (!isVideo(file)) {
+          return new Promise((resolve, reject) => {
+            axios.put(`${storageEndpoint}${file.hash}${file.ext}`,
+              file.buffer,
+              {
+                headers: {'AccessKey': config.storageApiKey}, maxContentLength: Infinity,
+                maxBodyLength: Infinity,
+              },)
+              .then(function (response) {
+                if (response.data.HttpCode === 201) {
+                  file.url = `/${config.pullZone}/${file.hash}${file.ext}`;
+                  resolve();
+                }
+              })
+              .catch(function (error) {
+                console.log(error);
 
-                resolve();
+                return reject(error);
+              });
+          });
+        } else {
+
+
+          return new Promise((resolve, reject) => {
+
+            let data;
+
+            const fileName = `${file.hash}${file.ext}`;
+
+            if (config.videostreamCollectionId != null) {
+              data = {
+                title: fileName,
+                collectionId: config.videostreamCollectionId
               }
-            })
-            .catch(function (error) {
-              console.log(error);
+            } else {
+              data = {
+                title: fileName
+              }
+            }
 
-              return reject(error);
-            });
-        });
-      },
+            axios.post(videoEndpoint,
+              data,
+              {headers: {'AccessKey': config.videostreamApiKey}})
+              .then(function (response) {
+                if (response.status === 200) {
+
+                  const guid = response.data.guid;
+
+                  axios.put(videoEndpoint + '/' + guid, file.buffer, {
+                    headers: {'AccessKey': config.videostreamApiKey, 'Content-Type': 'application/json'},
+                    maxBodyLength: Infinity,
+                  }).then(function (response) {
+                    file.url = `/${guid}/playlist.m3u8`;
+                    file.previewUrl = `/${guid}/preview.webp`;
+                    resolve();
+                  }).catch(function (error) {
+                    console.log(error);
+                    return reject(error);
+                  });
+                }
+              })
+              .catch(function (error) {
+                console.log(error);
+
+                return reject(error);
+              });
+          });
+
+        }
+      }
+      ,
       delete: async (file) => {
         return new Promise((resolve, reject) => {
-          axios.delete(`${storageEndpoint}${file.hash}${file.ext}`,
-            {headers: {'AccessKey': config.apiKey}})
-            .then(function (response) {
-              if (response.data.HttpCode === 200) {
-                resolve();
-              }
-            })
-            .catch(function (error) {
-              console.log(error);
+          if (!isVideo(file)) {
+            axios.delete(`${storageEndpoint}${file.hash}${file.ext}`,
+              {headers: {'AccessKey': config.storageApiKey}})
+              .then(function (response) {
+                if (response.data.HttpCode === 200) {
+                  resolve();
+                }
+              })
+              .catch(function (error) {
+                console.log(error);
 
-              return reject(error);
-            });
+                return reject(error);
+              });
+          } else {
+
+            const videoId = file.url.replace('/playlist.m3u8', '');
+
+            axios.delete(`${videoEndpoint}${videoId}`,
+              {headers: {'AccessKey': config.videostreamApiKey}})
+              .then(function (response) {
+
+                resolve();
+              })
+              .catch(function (error) {
+                console.log(error);
+
+                return reject(error);
+              });
+          }
         });
-      },
+      }
     };
   },
 };
