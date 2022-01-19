06e7fd4adce53d8ab8117c76dd30fa6f363ac351
Carmine Milieni <carmine.milieni@gmail.com>
2022-01-19 17:40:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -33,22 +33,22 @@ module.exports = {
         if (!isVideo(file)) {
           return new Promise((resolve, reject) => {
             axios.put(`${storageEndpoint}${file.hash}${file.ext}`,
-              file.buffer,
-              {
-                headers: {'AccessKey': config.storageApiKey}, maxContentLength: Infinity,
-                maxBodyLength: Infinity,
-              },)
-              .then(function (response) {
-                if (response.data.HttpCode === 201) {
-                  file.url = config.storageFolder != null ? `/${config.storageFolder}/${file.hash}${file.ext}` : `/${file.hash}${file.ext}`;
-                  resolve();
-                }
-              })
-              .catch(function (error) {
-                console.log(error);
+                file.buffer,
+                {
+                  headers: {'AccessKey': config.storageApiKey}, maxContentLength: Infinity,
+                  maxBodyLength: Infinity,
+                },)
+                .then(function (response) {
+                  if (response.data.HttpCode === 201) {
+                    file.url = config.storageFolder != null ? `/${config.storageFolder}/${file.hash}${file.ext}` : `/${file.hash}${file.ext}`;
+                    resolve();
+                  }
+                })
+                .catch(function (error) {
+                  console.log(error);
 
-                return reject(error);
-              });
+                  return reject(error);
+                });
           });
         } else {
 
@@ -71,31 +71,41 @@ module.exports = {
             }
 
             axios.post(videoEndpoint,
-              data,
-              {headers: {'AccessKey': config.videostreamApiKey}})
-              .then(function (response) {
-                if (response.status === 200) {
+                data,
+                {headers: {'AccessKey': config.videostreamApiKey}})
+                .then(function (response) {
+                  if (response.status === 200) {
 
-                  const guid = response.data.guid;
+                    const guid = response.data.guid;
 
-                  axios.put(videoEndpoint + '/' + guid, file.buffer, {
-                    headers: {'AccessKey': config.videostreamApiKey, 'Content-Type': 'application/json'},
-                    maxBodyLength: Infinity,
-                  }).then(function (response) {
-                    file.url = `/${guid}/playlist.m3u8`;
-                    file.previewUrl = `/${guid}/preview.webp`;
-                    resolve();
-                  }).catch(function (error) {
-                    console.log(error);
-                    return reject(error);
-                  });
-                }
-              })
-              .catch(function (error) {
-                console.log(error);
+                    axios.put(videoEndpoint + '/' + guid, file.buffer, {
+                      headers: {'AccessKey': config.videostreamApiKey, 'Content-Type': 'application/json'},
+                      maxBodyLength: Infinity,
+                    }).then(function (res) {
+                      axios.get(videoEndpoint + '/' + guid, {
+                        headers: {'AccessKey': config.videostreamApiKey, 'Content-Type': 'application/json'},
+                      }).then(function (r) {
+                        file.url = `/${guid}/playlist.m3u8`;
+                        file.previewUrl = `/${guid}/${r.data.thumbnailFileName}`;
+                        file.width = r.data.width;
+                        file.height = r.data.height;
+                        file.provider_metadata = {animationUrl:`/${guid}/preview.webp`,  ...r.data};
+                        resolve();
+                      }).catch(function (error){
+                        console.log(error);
+                        return reject(error);
+                      });
+                    }).catch(function (error) {
+                      console.log(error);
+                      return reject(error);
+                    });
+                  }
+                })
+                .catch(function (error) {
+                  console.log(error);
 
-                return reject(error);
-              });
+                  return reject(error);
+                });
           });
 
         }
@@ -105,32 +115,32 @@ module.exports = {
         return new Promise((resolve, reject) => {
           if (!isVideo(file)) {
             axios.delete(`${storageEndpoint}${file.hash}${file.ext}`,
-              {headers: {'AccessKey': config.storageApiKey}})
-              .then(function (response) {
-                if (response.data.HttpCode === 200) {
-                  resolve();
-                }
-              })
-              .catch(function (error) {
-                console.log(error);
+                {headers: {'AccessKey': config.storageApiKey}})
+                .then(function (response) {
+                  if (response.data.HttpCode === 200) {
+                    resolve();
+                  }
+                })
+                .catch(function (error) {
+                  console.log(error);
 
-                return reject(error);
-              });
+                  return reject(error);
+                });
           } else {
 
             const videoId = file.url.replace('/playlist.m3u8', '');
 
             axios.delete(`${videoEndpoint}${videoId}`,
-              {headers: {'AccessKey': config.videostreamApiKey}})
-              .then(function (response) {
+                {headers: {'AccessKey': config.videostreamApiKey}})
+                .then(function (response) {
 
-                resolve();
-              })
-              .catch(function (error) {
-                console.log(error);
+                  resolve();
+                })
+                .catch(function (error) {
+                  console.log(error);
 
-                return reject(error);
-              });
+                  return reject(error);
+                });
           }
         });
       }
